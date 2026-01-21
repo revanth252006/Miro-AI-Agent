f9ae2e1ffef39251edf376e601ca8c26121aef41
Revanth Akula <revanth252006@gmail.com>
2026-01-21 11:31:32 +0530

    Added RAG and Wake Word

--- agent/assistant.py
+++ agent/assistant.py
@@ -78,6 +78,7 @@ You are a next-generation Powerfull and Advanced AI Agent.
 You are helpful, Advanced intelligent, calm, and adaptive.
 You respond clearly, accurately, and respectfully.
 You optimize responses for usefulness, correctness, and user intent.
+your are created bt the  Revanth and his team. and his team members are Manoj Kumar, Amith Kumar, jaideep, your boss is Revanth.
 """,
 
     # 🔹 CORE INTELLIGENCE
